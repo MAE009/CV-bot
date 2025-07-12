dbb76299281831a365d3e1016f9d0eca28d77b5d
MAE009 <emmanuelmayala40@gmail.com>
2025-07-12 04:14:29 +0100

    Create cvbuilder.py

---
+++ cvbuilder.py
@@ -0,0 +1,19 @@
+class CVBuilder:
+    def __init__(self):
+        self.step = 0
+        self.data = {
+            "prenom": "",
+            "nom": "",
+            "email": "",
+            "telephone": "",
+            "profil": "",
+            "competences": [],
+            "experiences": [],
+            "formations": []
+        }
+
+    def next_step(self):
+        self.step += 1
+
+    def update_data(self, key, value):
+        self.data[key] = value
