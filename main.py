1d51fdc0ff6ef41cc9effd9d713bf7246e4fb9f1
MAE009 <emmanuelmayala40@gmail.com>
2025-07-12 15:34:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -75,18 +75,18 @@ async def event_CVbuilding(update: Update, context: ContextTypes.DEFAULT_TYPE):
             reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
             await update.message.reply_text("Quel est ton compte LinkedIn ou ton site web ?", reply_markup=reply_markup)
           
-            # Résumé de l’en-tête déjà rempli
-            await update.message.reply_text(
-            f"🧾 En-tête :\n"
-            f"{session.data['nom']} {session.data['prenom']}\n"
-            f"{session.data['ville']} || {session.data['tel']} || {session.data['email']} || {session.data.get('autre', 'N/A')}")
-
+            
             session.next_step()
 
     
 
     elif session.step == 6:
-        session.update_info("linkedin", update.message.text)
+        # Résumé de l’en-tête déjà rempli
+        await update.message.reply_text(
+            f"🧾 En-tête :\n"
+            f"{session.data['nom']} {session.data['prenom']}\n"
+            f"{session.data['ville']} || {session.data['tel']} || {session.data['email']} || {session.data.get('autre', 'N/A')}")
+
 
         await update.message.reply_text("Partie N° 2 : le résumé 📜")
 
