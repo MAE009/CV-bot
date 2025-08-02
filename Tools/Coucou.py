a8cedb840cbf67f3dd39e9d9495cbaa7c905967f
MAE009 <emmanuelmayala40@gmail.com>
2025-08-02 22:19:04 +0100

    Update Coucou.py

--- Tools/Coucou.py
+++ Tools/Coucou.py
@@ -44,28 +44,3 @@ def keep_alive(token, channel_id):
             await asyncio.sleep(120)  # ⏱️ 2 minutes
 
     asyncio.ensure_future(send_ping())
-
-
-"""    
-def keep_alive(token, channel_id):
-    from telegram import Bot
-    import threading
-
-    bot = Bot(token)
-    message = random.choice(MESSAGES)
-    bot.send_message(chat_id=channel_id, text=message)
-
-    # Relancer après 10 minutes
-    threading.Timer(600, keep_alive, args=(token, channel_id)).start()
-
-
-
-import threading
-from telegram import Bot
-
-def keep_alive(bot_token, channel_id):
-    bot = Bot(token=bot_token)
-    bot.send_message(chat_id=channel_id, text="✅ Bot actif : ping toutes les 28 min.")
-    # Relancer la fonction dans 1680 secondes (28 min)
-    threading.Timer(1680, keep_alive, args=[bot_token, channel_id]).start()
-"""
