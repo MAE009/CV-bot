92dc90d06b270ab273461d3200bd4985e47e1099
MAE009 <emmanuelmayala40@gmail.com>
2025-08-08 20:31:13 +0100

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -2,7 +2,7 @@ from .cv_handlers import setup_cv_handlers
 from .menu_handlers import setup_menu_handlers
 from .models_handlers import setup_models_handlers
 
-def setup_handlers(app):
+async def setup_handlers(app):
     setup_menu_handlers(app)
     setup_cv_handlers(app)
     setup_models_handlers(app)
