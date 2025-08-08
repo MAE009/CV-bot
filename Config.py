1400e136663573031c8d5b79d9cfec32b01afb99
MAE009 <emmanuelmayala40@gmail.com>
2025-08-08 19:32:14 +0100

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,6 +1,8 @@
+import os
 # ====================
 # 🌐 Variables Globales
 # ====================
 sessions = {}  # Stocke les sessions utilisateur
 YOUR_USER_ID = 5227032520  # Votre ID
+token = os.getenv("TELEGRAM_BOT_TOKEN")
 CHANNEL_ID = "@Temoignage_Service_M_A_E005"  # Canal Telegram
