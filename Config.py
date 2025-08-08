35cfc5c7f3962fe03d877386759704af1060b5b2
MAE009 <emmanuelmayala40@gmail.com>
2025-08-08 19:03:24 +0100

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,6 @@
+# ====================
+# 🌐 Variables Globales
+# ====================
+sessions = {}  # Stocke les sessions utilisateur
+YOUR_USER_ID = 5227032520  # Votre ID
+CHANNEL_ID = "@Temoignage_Service_M_A_E005"  # Canal Telegram
