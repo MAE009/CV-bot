effe2522ecc00e67e7a95c5b20f1f90a2355f71a
MAE009 <emmanuelmayala40@gmail.com>
2025-09-26 16:55:04 +0100

    Update models_handlers.py

--- handlers/models_handlers.py
+++ handlers/models_handlers.py
@@ -44,7 +44,7 @@ async def modele_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await query.answer()
 
     try:
-        await update.message.reply_text(f"DEBUG query.data = {query.data}")  # 🔍 Debug
+        await query.edit_message_text(f"DEBUG query.data = {query.data}")  # 🔍 Debug
 
         parts = query.data.split("|")
         cv_type = parts[0]
