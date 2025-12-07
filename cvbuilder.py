520c78a0ff9a407966d5638440e3270adcb87e06
MAE009 <emmanuelmayala40@gmail.com>
2025-12-07 10:49:03 +0100

    Update test data for modern CV generator
    Changed test data source for modern CV generation.

--- cvbuilder.py
+++ cvbuilder.py
@@ -191,7 +191,7 @@ class CVBuilder:
         """
         Génère un CV moderne et son image LinkedIn
         """
-        data = test_data_duc
+        data = test_data_emmanuel
         try:
             # 1️⃣ Préparation de la photo
             with open(data['photo_path'], "rb") as f:
